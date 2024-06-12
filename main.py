2e0a285d102cd6de5182e529ce1ec86349db94a8
EinanCO <92308515+EinanCO@users.noreply.github.com>
2024-06-12 18:50:08 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pyautogui
+import time
+
+
+# time.sleep(2)
+#print(pyautogui.size())
+# print(pyautogui.position())
+# pyautogui.moveTo(1554,677,3)
+# pyautogui.doubleClick()
+
+# pyautogui.write("hello")
+
+pyautogui.hotkey('command','c',interval=0.5)
+
+
+
+ 
