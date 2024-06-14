f8c29e093aa6a44e879303e8f8e4df7e13416f70
EinanCO <92308515+EinanCO@users.noreply.github.com>
2024-06-14 18:17:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import pyautogui
 import time
+import tkinter as tk
 
+def clickCopy():
+    time.sleep(0.1)
+    pyautogui.hotkey('command', 'tab')
+    time.sleep(0.1)
+    pyautogui.hotkey('command', 'c', interval=0.5)
 
-# time.sleep(2)
-#print(pyautogui.size())
-# print(pyautogui.position())
-# pyautogui.moveTo(1554,677,3)
-# pyautogui.doubleClick()
+root = tk.Tk()
+root.title("talSystem")
+root.geometry("600x200")
 
-# pyautogui.write("hello")
+buttonFrame = tk.Frame(root)
+buttonFrame.columnconfigure(0, weight=1)
+buttonFrame.columnconfigure(1, weight=1)
 
-pyautogui.hotkey('command','c',interval=0.5)
+button1 = tk.Button(buttonFrame, text="Copy", font=('arial', 10), command=clickCopy)
+button1.grid(row=0, column=0, sticky=tk.W + tk.E)
+
+button2 = tk.Button(buttonFrame, text="", font=('arial', 10))
+button2.grid(row=0, column=1, sticky=tk.W + tk.E)
+
+button3 = tk.Button(buttonFrame, text="", font=('arial', 10))
+button3.grid(row=1, column=0, sticky=tk.W + tk.E)
+
+button4 = tk.Button(buttonFrame, text="", font=('arial', 10))
+button4.grid(row=1, column=1, sticky=tk.W + tk.E)
+
+buttonFrame.pack(fill='x')
+
+root.mainloop()
+
