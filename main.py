e3868a04242f132d22b9abf8261e2245d2bb102b
EinanCO <92308515+EinanCO@users.noreply.github.com>
2024-06-23 08:12:37 +0300

    fix grid and window location

--- main.py
+++ main.py
@@ -4,32 +4,32 @@ import tkinter as tk
 
 def clickCopy():
     time.sleep(0.1)
-    pyautogui.hotkey('command', 'tab')
+    pyautogui.hotkey('alt', 'tab')
     time.sleep(0.1)
-    pyautogui.hotkey('command', 'c', interval=0.5)
+    pyautogui.hotkey('ctrl', 'c', interval=0.5)
 
 root = tk.Tk()
 root.title("talSystem")
-root.geometry("600x200")
+root.geometry("600x200+1300+10")
 
 buttonFrame = tk.Frame(root)
 buttonFrame.pack(fill='both', expand=True)
 
 buttonFrame.columnconfigure(0, weight=1)
 buttonFrame.columnconfigure(1, weight=1)
+buttonFrame.rowconfigure(0,weight=1)
+buttonFrame.rowconfigure(1,weight=1)
 
 button1 = tk.Button(buttonFrame, text="Copy", font=('arial', 10), command=clickCopy)
-button1.grid(row=0, column=0, sticky=tk.W + tk.E)
+button1.grid(row=0, column=0, sticky=tk.NSEW)
 
 button2 = tk.Button(buttonFrame, text="", font=('arial', 10))
-button2.grid(row=0, column=1, sticky=tk.W + tk.E)
+button2.grid(row=0, column=1, sticky=tk.NSEW)
 
 button3 = tk.Button(buttonFrame, text="", font=('arial', 10))
-button3.grid(row=1, column=0, sticky=tk.W + tk.E)
+button3.grid(row=1, column=0, sticky=tk.NSEW)
 
 button4 = tk.Button(buttonFrame, text="", font=('arial', 10))
-button4.grid(row=1, column=1, sticky=tk.W + tk.E)
+button4.grid(row=1, column=1, sticky=tk.NSEW)
 
 root.mainloop()
-
-#lalalalala
