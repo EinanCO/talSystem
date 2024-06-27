3be8ed7a3892d177fbb169ccdbdf9b319761fb40
EinanCO <92308515+EinanCO@users.noreply.github.com>
2024-06-27 07:47:39 +0300

    update grid

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def clickCopy():
     time.sleep(0.1)
     pyautogui.hotkey('alt', 'tab')
     time.sleep(0.1)
-    pyautogui.hotkey('ctrl', 'c', interval=0.5)
+    pyautogui.hotkey('ctrl', 'c')
 
 root = tk.Tk()
 root.title("talSystem")
@@ -17,19 +17,28 @@ buttonFrame.pack(fill='both', expand=True)
 
 buttonFrame.columnconfigure(0, weight=1)
 buttonFrame.columnconfigure(1, weight=1)
+buttonFrame.columnconfigure(2, weight=1)
+buttonFrame.columnconfigure(3, weight=1)
 buttonFrame.rowconfigure(0,weight=1)
 buttonFrame.rowconfigure(1,weight=1)
 
-button1 = tk.Button(buttonFrame, text="Copy", font=('arial', 10), command=clickCopy)
-button1.grid(row=0, column=0, sticky=tk.NSEW)
+button1 = tk.Button(buttonFrame, text="\u2190", font=('arial', 60))
+button1.grid(row=0, column=0,rowspan=2, sticky=tk.NSEW)
 
-button2 = tk.Button(buttonFrame, text="", font=('arial', 10))
-button2.grid(row=0, column=1, sticky=tk.NSEW)
+button2 = tk.Button(buttonFrame, text="\u2192", font=('arial', 60))
+button2.grid(row=0, column=1,rowspan=2, sticky=tk.NSEW)
 
-button3 = tk.Button(buttonFrame, text="", font=('arial', 10))
-button3.grid(row=1, column=0, sticky=tk.NSEW)
+button3 = tk.Button(buttonFrame, text="Copy", font=('arial', 10), command=clickCopy)
+button3.grid(row=0, column=2, sticky=tk.NSEW)
 
-button4 = tk.Button(buttonFrame, text="", font=('arial', 10))
-button4.grid(row=1, column=1, sticky=tk.NSEW)
+button4 = tk.Button(buttonFrame, text="Insert", font=('arial', 10))
+button4.grid(row=0, column=3, sticky=tk.NSEW)
+
+button4 = tk.Button(buttonFrame, text="Mark-in", font=('arial', 10))
+button4.grid(row=1, column=2, sticky=tk.NSEW)
+
+button4 = tk.Button(buttonFrame, text="Mark-out", font=('arial', 10))
+button4.grid(row=1, column=3, sticky=tk.NSEW)
 
 root.mainloop()
+
