3b5c94fb24ac966ad9df5957de52ad9c7717ab6b
liraz4444 <lirazakiva4444@gmail.com>
2024-06-29 15:07:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ def clickCopy():
     time.sleep(0.1)
     pyautogui.hotkey('ctrl', 'c')
 
+def rightClick():
+    time.sleep(0.1)
+    pyautogui.hotkey('alt', 'tab')
+    # time.sleep(0.1)
+    # pyautogui.press('i')
+    time.sleep(0.1)
+    pyautogui.press('space')
+    time.sleep(0.1)
+    pyautogui.press('down')
+
+
+
+
+
+
+
 root = tk.Tk()
 root.title("talSystem")
 root.geometry("600x200+1300+10")
@@ -25,7 +41,7 @@ buttonFrame.rowconfigure(1,weight=1)
 button1 = tk.Button(buttonFrame, text="\u2190", font=('arial', 60))
 button1.grid(row=0, column=0,rowspan=2, sticky=tk.NSEW)
 
-button2 = tk.Button(buttonFrame, text="\u2192", font=('arial', 60))
+button2 = tk.Button(buttonFrame, text="\u2192", font=('arial', 60), command=rightClick)
 button2.grid(row=0, column=1,rowspan=2, sticky=tk.NSEW)
 
 button3 = tk.Button(buttonFrame, text="Copy", font=('arial', 10), command=clickCopy)
