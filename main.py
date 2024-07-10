52cc871cdef0c92e4fc0b433a51b939e3f0fd9ba
liraz4444 <lirazakiva4444@gmail.com>
2024-07-10 20:31:08 +0300

    add cut & delete

--- main.py
+++ main.py
@@ -2,28 +2,37 @@ import pyautogui
 import time
 import tkinter as tk
 
-def clickCopy():
-    time.sleep(0.1)
-    pyautogui.hotkey('alt', 'tab')
-    time.sleep(0.1)
-    pyautogui.hotkey('ctrl', 'c')
+# def clickCopy():
+#     time.sleep(0.1)
+#     pyautogui.hotkey('alt', 'tab')
+#     time.sleep(0.1)
+#     pyautogui.hotkey('ctrl', 'c')
 
 def rightClick():
     time.sleep(0.1)
     pyautogui.hotkey('alt', 'tab')
-    pyautogui.click('C:/Users/7352297/Documents/GitHub/talSystem/right_click.png')
-
-def playClick():
     time.sleep(0.1)
-    pyautogui.hotkey('alt', 'tab')
-    pyautogui.click('C:/Users/7352297/Documents/GitHub/talSystem/play.png')
+    pyautogui.press('right')
 
 def leftClick():
     time.sleep(0.1)
     pyautogui.hotkey('alt', 'tab')
-    pyautogui.click('C:/Users/7352297/Documents/GitHub/talSystem/left_click.png')
+    time.sleep(0.1)
+    pyautogui.press('left')
 
+def cutClick():
+    time.sleep(0.1)
+    pyautogui.hotkey('alt', 'tab')
+    time.sleep(0.1)
+    pyautogui.hotkey('ctrl', 'k')
 
+def deleteClick():
+    time.sleep(0.1)
+    pyautogui.hotkey('alt', 'tab')
+    time.sleep(0.1)
+    pyautogui.press('d')
+    time.sleep(0.1)
+    pyautogui.hotkey('shift', 'delete')
 
 
 root = tk.Tk()
@@ -46,20 +55,25 @@ leftClickButton.grid(row=0, column=0,rowspan=2, sticky=tk.NSEW)
 rightClickButton = tk.Button(buttonFrame, text="\u2192", font=('arial', 60), command=rightClick)
 rightClickButton.grid(row=0, column=1,rowspan=2, sticky=tk.NSEW)
 
-button3 = tk.Button(buttonFrame, text="Copy", font=('arial', 10), command=clickCopy)
-button3.grid(row=0, column=2, sticky=tk.NSEW)
+deleteClickButton = tk.Button(buttonFrame, text="delete", font=('arial', 10),command=deleteClick)
+deleteClickButton.grid(row=0, column=3, sticky=tk.NSEW)
 
-button4 = tk.Button(buttonFrame, text="Insert", font=('arial', 10))
-button4.grid(row=0, column=3, sticky=tk.NSEW)
+cutClickButton = tk.Button(buttonFrame, text="cut", font=('arial', 10),command=cutClick)
+cutClickButton.grid(row=1, column=2, sticky=tk.NSEW)
 
-button4 = tk.Button(buttonFrame, text="Mark-in", font=('arial', 10))
-button4.grid(row=1, column=2, sticky=tk.NSEW)
 
-button4 = tk.Button(buttonFrame, text="Mark-out", font=('arial', 10))
-button4.grid(row=1, column=3, sticky=tk.NSEW)
+# cutClickButton = tk.Button(buttonFrame, text="cut", font=('arial', 10),command=cutClick)
+# cutClickButton.grid(row=0, column=3, sticky=tk.NSEW)
 
-playClickButton = tk.Button(buttonFrame, text="play", font=('arial', 10), command=playClick)
-playClickButton.grid(row=1, column=3, sticky=tk.NSEW)
+# button4 = tk.Button(buttonFrame, text="Mark-out", font=('arial', 10))
+# button4.grid(row=1, column=3, sticky=tk.NSEW)
+
+
+# button3 = tk.Button(buttonFrame, text="Copy", font=('arial', 10), command=clickCopy)
+# button3.grid(row=0, column=2, sticky=tk.NSEW)
+
+# playClickButton = tk.Button(buttonFrame, text="play", font=('arial', 10), command=playClick)
+# playClickButton.grid(row=1, column=3, sticky=tk.NSEW)
 
 
 root.mainloop()
